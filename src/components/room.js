aa433121dac188c01aec8c4c6a7463d67c2fe9b5
ckinmind <245160421@qq.com>
2017-02-25 20:59:25 +0800

    调整room组件，发现一个暂停时颜色改变的bug

--- src/components/room.js
+++ src/components/room.js
@@ -9,7 +9,8 @@ class Room extends React.Component {
     constructor(props){
         super(props);
         this.lvMap = [2, 3, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9]; /*存储对应索引关卡的一行span数目*/
-        this.offset = 0;
+      //  this.offset = 0;
+      //   this.degree = 0;                                                  /* 当前关卡的一行数目, 对应lvMap[lv]*/
         this.state = {
             isPause: false,
             isGameOver: false,
@@ -41,23 +42,21 @@ class Room extends React.Component {
         }
     }
 
+    getDegree(){
+        return this.lvMap[this.state.lv] || 9;  /* 获取一行的span数目值，如果没有则为9*/
+    }
+
     /** 获取游戏span数组 */
-    // getSpanArr(){
-    //     let lv = this.state.lv;                                          /* 获取当前等级(也是积分) */
-    //     let degree = this.lvMap[lv] || 9;                                /* 获取一行的span数目值，如果没有则为9*/
-    //     this.offset = getOffset(degree, lv);                             /* 获取偏离数值 */
-    //     let { color, offsetColor } = getColor(this.offset);              /* 获取正常颜色和偏离颜色 */
-    //     let spanArr = getSpan(degree, color, offsetColor);               /* 获取所有的span */
-    //     return spanArr;
-    // }
+    getSpanArr(){
+        let lv = this.state.lv;                                     /* 获取当前等级(也是积分) */
+        let degree = this.getDegree();                              /* 获取一行的span数目*/
+        let offset = getOffset(degree, lv);                         /* 获取偏离数值 */
+        let { color, offsetColor } = getColor(offset);              /* 获取正常颜色和偏离颜色 */
+        let spanArr = getSpan(degree, color, offsetColor);          /* 获取所有的span */
+        return spanArr;
+    }
 
     render(){
-
-        let lv = this.state.lv;                                          /* 获取当前等级(也是积分) */
-        let degree = this.lvMap[lv] || 9;                                /* 获取一行的span数目值，如果没有则为9*/
-        this.offset = getOffset(degree, lv);                             /* 获取偏离数值 */
-        let { color, offsetColor } = getColor(this.offset);              /* 获取正常颜色和偏离颜色 */
-        let spanArr = getSpan(degree, color, offsetColor);               /* 获取所有的span */
 
         return (
             <div className="page" id="room">
@@ -67,8 +66,8 @@ class Room extends React.Component {
                     <span className="btn btn-pause" onClick={this.showGamePause.bind(this)}>暂停</span>
                 </header>
 
-                <div id="box" className={`lv${degree}`} style={{width: '500px', height: '500px'}}  onClick={this.handleClick.bind(this)}>
-                    { spanArr }
+                <div id="box" className={`lv${this.getDegree()}`} style={{width: '500px', height: '500px'}}  onClick={this.handleClick.bind(this)}>
+                    { this.getSpanArr() }
                 </div>
 
                 <Pause isPause={this.state.isPause} gameGoOn={this.gameGoOn.bind(this)} rePlay={this.props.rePlay}/>
